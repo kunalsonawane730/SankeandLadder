571c56baddb0efdf9e83ea49bed9beb4925f2088
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-06 16:18:06 +0530

    [kunal]commiting changes for UC1 position zero of Player1

---
+++ SnakeandLadders/SnakeandLadders/GameSnakeandLadder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadders
+{
+    public class SnakeandLadder
+    {
+        //UC-1
+        public void GameSnakeandLadder()
+        {
+            int startPosition = 0;
+            int positionPlyaer1;
+            Console.WriteLine("Game is start");
+            Console.WriteLine("Player1 start position is " + startPosition);
+        }
+        
+    }
+}
---
+++ SnakeandLadders/SnakeandLadders/Program.cs
@@ -0,0 +1,7 @@
+﻿using SnakeandLadders;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//UC-1
+SnakeandLadder game = new SnakeandLadder();
+game.GameSnakeandLadder();
