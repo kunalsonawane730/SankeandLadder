4312bde9624e925926b1acc15c11e6bf6ea9ff3c
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-06 16:58:05 +0530

    [kunal]commiting changes of optionforplayer usecase

--- SnakeandLadders/SnakeandLadders/GameSnakeandLadder.cs
+++ SnakeandLadders/SnakeandLadders/GameSnakeandLadder.cs
@@ -8,18 +8,39 @@ namespace SnakeandLadders
 {
     public class SnakeandLadder
     {
-    //UC-1
+        const int No_Play = 0; 
+        const int Ladder = 1; 
+        const int Snake = 2;
+        int PlayerPosition = 0;
+        //UC-1 start psition o
         public void GameSnakeandLadder()
         {
             int startPosition = 0;
-            int positionPlyaer1;
             Console.WriteLine("Game is start");
-            Console.WriteLine("Player1 start position is " + startPosition);
-    //UC-2
+            Console.WriteLine("Player start position is " + startPosition);
+
+    //UC-2 Dice number result
             Random random = new Random();
             int diceNumber = random.Next(1, 7);
             Console.WriteLine("Rolled dice number is: " + diceNumber);
 
+    //UC-3 Option for Player
+            int option = random.Next(0, 3);
+
+            switch (option)
+            {
+                case No_Play:
+                    break;
+
+                case Ladder:
+                    PlayerPosition += diceNumber;
+                    break;
+
+                case Snake:
+                    PlayerPosition -= diceNumber;
+                    break;
+            }
+            Console.WriteLine("Option is: " + option + "\nNow Player position is: " + PlayerPosition);
         }
     }
 }
