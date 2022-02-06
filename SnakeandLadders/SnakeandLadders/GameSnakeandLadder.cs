bdcca50fb2f31fde047a56eeb1d1f85b0cd95e64
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-06 16:29:09 +0530

    [kunal]commiting changes for dice number result program

--- SnakeandLadders/SnakeandLadders/GameSnakeandLadder.cs
+++ SnakeandLadders/SnakeandLadders/GameSnakeandLadder.cs
@@ -8,14 +8,18 @@ namespace SnakeandLadders
 {
     public class SnakeandLadder
     {
-        //UC-1
+    //UC-1
         public void GameSnakeandLadder()
         {
             int startPosition = 0;
             int positionPlyaer1;
             Console.WriteLine("Game is start");
             Console.WriteLine("Player1 start position is " + startPosition);
+    //UC-2
+            Random random = new Random();
+            int diceNumber = random.Next(1, 7);
+            Console.WriteLine("Rolled dice number is: " + diceNumber);
+
         }
-        
     }
 }
